96f091d7c928b64bd194dfaedf4aa76eeb87e7fc
Neelima-git <bellana.neelima2@gmail.com>
2022-03-05 19:37:27 +0530

    Fixed ci script

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-function CalculateSI()
-{
-    let p=document.getElementById('amount').value;
-    let t=document.getElementById('time').value;
-    let r=document.getElementById('rate').value;
-
-    let  SI=(p*t*r)/100;
-
-    document.getElementById('si').innerHTML="The Simple Interest is: "+SI;
-}
-
-function CalculateCI(){
-
-    p =document.getElementById("principal").value;
-    r =document.getElementById("annual-interest-rate").value;
-    t =document.getElementById("number-of-years").value;
-    n =document.getElementById("compounding-frequency").value;
-
-    let r1 = r/100;
-    let nbr=r1/n; // rate of interest / no of years
-    let x = (1+nbr);
-    let nt = n*t;
-    let xnt = Math.pow(x,nt); // Function used to calculate the power
-    let CI = parseInt(p*xnt);
-    
-    document.getElementById('ci').innerHTML="The Compound Interest is: "+CI;
-}
+function CalculateSI() {
+    let p = document.getElementById("amount").value;
+    let t = document.getElementById("time").value;
+    let r = document.getElementById("rate").value;
+  
+    let SI = (p * t * r) / 100;
+  
+    document.getElementById("si").innerHTML = "The Simple Interest is: " + SI;
+  }
+  
+  function CalculateCI() {
+    var p = document.getElementById("principal").value;
+    var R = document.getElementById("annual-interest-rate").value;
+    var t = document.getElementById("number-of-years").value;
+    var cf = document.getElementById("compounding-frequency").value;
+    var n = 0;
+    if (cf == "Yearly") n = 1;
+    else if (cf == "Half Yearly") n = 2;
+    else if (cf == "Quaterly") n = 4;
+    else if (cf == "Monthly") n = 12;
+  
+    var r = R / 100;
+    var nt = n * t;
+    var CI = p * (Math.pow(1 + r / n, nt) - 1);
+    document.getElementById("ci").innerHTML =
+      "The Compound Interest is: " + CI.toFixed(2);
+  }
+  
